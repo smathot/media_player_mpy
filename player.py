b50bca37aea810171909ef9e1cbe242e5e81169b
Daniel Schreij <dschreij@gmail.com>
2015-09-28 23:09:03 +0200

    updates

--- player.py
+++ player.py
@@ -1,6 +1,15 @@
-from moviepy.video.io import VideoFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
 
 class Player(object):
 
-	def __init__(self):
-		pass
+	def __init__(self, videofile=None):
+		self.load_video(videofile)
+		
+		
+	def load_video(self, videofile):
+		if not videofile is None:
+			if os.path.exists(videofile):
+				self.clip = VideoFileClip(videofile)
+			else:
+				raise IOError("File not found: {0}".format(videofile))
